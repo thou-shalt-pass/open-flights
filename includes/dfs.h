51a844f0a07580e65320c9842ba3701147679627
Yirong Chen <yirongc3@illinois.edu>
2022-04-16 05:25:22 +0000

    feat: dfs

--- includes/dfs.h
+++ includes/dfs.h
@@ -1,15 +1,49 @@
 #ifndef _DFS_H
 #define _DFS_H
 
+#include <stack>
+
 #include "type.h"
 
 /**
  * @brief abstract DFS operation
  * 
  * @param graph adjacency list
- * @param operation function object of the operation
+ * @param look_next_origin function object that 
+ * return the next node in the origin considering sequence; 
+ * return a number greater than or equal to the graph size to stop DFS 
+ * @param op_before_component function object that 
+ * op_before_component(origin_idx) is called before the start of component traversal; 
+ * return component_handle that will be passed into op_after_pop
+ * @param op_start_visit function object that 
+ * op_start_visit(curr_node_idx, component_handle) is called after the node is poped from the stack
+ * @param op_after_visit function object that 
+ * op_after_visit(curr_node_idx, component_handle) is called after the node is finished from visiting
  */
-template <typename Op>
-void DFS(const AdjList& graph, Op operation);
+template <typename LookNextOrigin, typename OpBeforeComponent, typename OpBeforeVisit, typename OpAfterVisit>
+void DFS(const AdjList& graph, LookNextOrigin look_next_origin, 
+        OpBeforeComponent op_before_component, OpBeforeVisit op_start_visit, OpAfterVisit op_after_visit) {
+    size_t n = graph.size(), origin;
+    std::vector<bool> visited(graph.size(), false);
+    while ((origin = look_next_origin()) < n) {
+        if (visited[origin]) { continue; }
+        visited[origin] = true;
+        int component_handle = op_before_component(origin);
+        std::stack<size_t> s;
+        s.push(origin);
+        while (!s.empty()) {
+            size_t u = s.top();
+            s.pop();
+            op_start_visit(u, component_handle);
+            for (size_t v : graph[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    s.push(v);
+                }
+            }
+            op_after_visit(u, component_handle);
+        }
+    }
+}
 
 #endif
