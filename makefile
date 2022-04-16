c7112a2bd5ea5c3a20547c09fd5f12a041f0ff54
Yirong Chen <yirongc3@illinois.edu>
2022-04-16 06:02:20 +0000

    add: demo of scc and importance

--- makefile
+++ makefile
@@ -7,7 +7,7 @@ tests_importance: bin/tests_importance
 tests_matrix_operation: bin/tests_matrix_operation
 tests_strongly_connected_components: bin/tests_strongly_connected_components
 
-bin/main: ./src/data.cpp ./src/main.cpp 
+bin/main: ./src/data.cpp ./src/main.cpp ./src/strongly_connected_components.cpp ./src/importance.cpp
 	$(CXX) $(CXXFLAGS) $^ -o $@
 bin/tests_importance: obj/catch.o ./tests/tests_utilities.cpp ./tests/tests_importance.cpp ./src/importance.cpp
 	$(CXX) $(CXXFLAGS) $^ -o $@
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-#include "data.h"
 #include <iostream>
+#include <numeric>
+
+#include "data.h"
+#include "importance.h"
+#include "strongly_connected_components.h"
 
 int main() {
     Data data("data/airport.csv", "data/route.csv");
+    const AdjList& adj_list = data.GetAdjList();
+
+    std::list<std::list<size_t> > scc = StronglyConnectedComponents(adj_list);
+    // std::list<size_t>* largest_scc;
+    // size_t largest_scc_size = 0;
+    // for (std::list<size_t>& x : scc) {
+    //     if (x.size() > largest_scc_size) {
+    //         largest_scc_size = x.size();
+    //         largest_scc = &x;
+    //     }
+    // }
+
+    std::vector<double> importance = Importance(adj_list, 1);
+    // std::cout << std::accumulate(importance.begin(), importance.end(), 0.0) << std::endl;
+
     return 0;
 }
