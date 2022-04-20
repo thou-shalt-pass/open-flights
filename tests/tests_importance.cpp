2dde0071b6872694ff9a30bd63c89641f22877a4
Yirong Chen <yirongc3@illinois.edu>
2022-04-20 00:30:26 +0000

    tests: PageRank it ver edge cases

--- tests/tests_importance.cpp
+++ tests/tests_importance.cpp
@@ -15,26 +15,33 @@ TEST_CASE("Importance(const AdjList& graph)", "[importance_adj_list]") {
         std::vector<double> importance = Importance(graph, 10);
         CheckVectorDouble(importance, { 0.38, 0.12, 0.29, 0.19 }, 0.01);
     }
-    // SECTION("nodes with no out-going edges") {
-    //     AdjList graph {
-    //         { 2 },
-    //         { 2 },
-    //         { }
-    //     };
-    //     std::vector<double> importance = Importance(graph, 10);
-    //     CheckVectorDouble(importance, { 0, 0, 0 }, 0.01);
-    // }
-    // SECTION("disconnected") {
-    //     AdjList graph {
-    //         { 1 },
-    //         { 0 },
-    //         { 3, 4 },
-    //         { 2, 4 },
-    //         { 2, 3 }
-    //     };
-    //     std::vector<double> importance_a = Importance(graph, 10);
-    //     std::vector<double> importance_b = Importance(graph, 11);
-    //     CheckVectorDouble(importance_a, { 0, 0, 1, 1, 1 }, 0.01);
-    //     CheckVectorDouble(importance_b, { 1, 1, 0, 0, 0 }, 0.01);
-    // }
+    SECTION("nodes with no out-going edges 1") {
+        AdjList graph {
+            { 2 },
+            { 2 },
+            { }
+        };
+        std::vector<double> importance = Importance(graph, 10);
+        CheckVectorDouble(importance, { 0, 0, 0 }, 0.01);
+    }
+    SECTION("nodes with no out-going edges 2") {
+        AdjList graph {
+            { 1 },
+            { 0 },
+            { }
+        };
+        std::vector<double> importance = Importance(graph, 10);
+        CheckVectorDouble(importance, { 0.33, 0.33, 0 }, 0.01);
+    }
+    SECTION("disconnected") {
+        AdjList graph {
+            { 1 },
+            { 0 },
+            { 3, 4 },
+            { 2, 4 },
+            { 2, 3 }
+        };
+        std::vector<double> importance_a = Importance(graph, 10);
+        CheckVectorDouble(importance_a, { 0.2, 0.2, 0.2, 0.2, 0.2 }, 0.01);
+    }
 }
